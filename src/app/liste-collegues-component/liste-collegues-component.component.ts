a9d47355a92b729b750e2dad9f3e61ab6ef9adde
yhando24 <youcef.handoura@gmail.com>
2019-03-26 11:57:48 +0100

    fin top collegue 2 debut directives edit vue

---
+++ src/app/liste-collegues-component/liste-collegues-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-collegues-component',
+  templateUrl: './liste-collegues-component.component.html',
+  styleUrls: ['./liste-collegues-component.component.css']
+})
+export class ListeColleguesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
