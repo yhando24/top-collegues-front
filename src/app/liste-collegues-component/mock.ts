949d36659b61d6380dd5de3a1b51a4365dff8302
yhando24 <youcef.handoura@gmail.com>
2019-04-01 13:57:35 +0200

    gestion asynchrone v2

--- src/app/liste-collegues-component/mock.ts
+++ src/app/liste-collegues-component/mock.ts
@@ -3,43 +3,4 @@ import { Collegue } from './../models';
 
 
 export const listeCollegues: Collegue[] = [
-    {
-    pseudo :'youcef',
-    score : 200,
-    photoUrl: 'https://media.licdn.com/dms/image/C4E03AQFlHrZpdWLrCg/profile-displayphoto-shrink_200_200/0?e=1559174400&v=beta&t=x_w8Y3288kZDjGLS-Yhs9TTQpy25iOBynGdwhZga69A'
-    },
-    {
-    pseudo: 'yoann',
-    score : 300,
-    photoUrl: 'https://media.licdn.com/dms/image/C4D03AQGbtRrqkhoGCA/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=M9y5GcnnnFNfSst9CUZsB8ZLmuFaiYtamA-psGaeDWI'
-
-    },
-     {
-        pseudo: 'anissKiller',
-        score: 400,
-         photoUrl: 'https://media.licdn.com/dms/image/C4D03AQFUqUHGL3OvOg/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=DD_8FhQ6Zm-JeaJ0m3k4WKQhrGs-5H0Dxe1xxfjCtSM'
-
-    },
-  {
-    pseudo: 'amandine',
-    score: 400,
-    photoUrl: 'https://media.licdn.com/dms/image/C4E03AQFDrBClEjIJ5Q/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=bPJqEQtx20CsErRxMxhdMm0jZ68FCY81gm8X5akFhCg'
-
-  },
-  {
-    pseudo: 'alexander',
-    score: 400,
-    photoUrl: 'https://media.licdn.com/dms/image/C5603AQGMjBZT9ZnJrQ/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=tM6q7r7ei7yalNZlXFXI2DJxyqZ7onQU_gFpIBJ944U'
-
-  },
-  {
-    pseudo: 'melanie',
-    score: 400,
-    photoUrl: 'https://media.licdn.com/dms/image/C5603AQGUtyWY-7siYQ/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=iynbCCh1z0mdy37C4sVQJn4jfDO3xg2HkiweZtDlaGI'
-  },
-  {
-    pseudo: 'mel',
-    score: 400,
-    photoUrl: 'https://media.licdn.com/dms/image/C5603AQGvKai-t2qzfg/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=IaNdrwdrMd798e3iinwGnGYn6ho4ZxbtH_wZ4eBwSxU'
-  }
 ]
